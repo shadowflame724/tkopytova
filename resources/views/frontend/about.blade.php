3b47d29737b4803f0f6d8e5931a6c00bd68d2d92
alexkopytov <alex@top-agt.com>
2018-12-05 00:14:49 +0200

    about

--- resources/views/frontend/about.blade.php
+++ resources/views/frontend/about.blade.php
@@ -3,8 +3,30 @@
 @section('content')
     <div class="album py-5">
         <div class="container">
-            <div class="row">
-                ABOUT INFO
+            <div class="row d-flex flex-column about-container justify-content-center align-items-center">
+                <div class="about-image-container d-flex justify-content-center">
+                    <img src="/img/frontend/about_image.jpg"  style="max-width: 100%;" alt="my photo">
+                </div>
+                <div class="about-description-container flex-column d-flex justify-content-center">
+                    <div class="">
+                        Hello! My name is Tetiana. I have two passions: children’s illustration and painting horses.
+                    </div>
+                    <div class="">
+                        I am a professional artist and book illustrator. I have master's degree in arts. My main
+                        specializations are 2D graphic and illustration. I have 6+ years’ experience as a game artist and
+                        10+ years’ experience as a children's books illustrator. More than 15 books illustrated by me were
+                        published. I do both digital (Adobe Photoshop) and watercolor illustrations.
+                    </div>
+                    <div class="">
+                        My second passion is painting horses. I specialize on oil paintings on canvas and graphic drawings.
+                        My art pieces were showed in several exhibitions; my horse paintings are in private collections all
+                        over the world. Horses are my inspiration and a big part of my life.
+                    </div>
+                    <div class="">
+                        I’m always looking for new creative projects. Please contact me by mail: <a href="mailto:art_inside@ukr.net">art_inside@ukr.net</a> of find
+                        me in social media.
+                    </div>
+                </div>
             </div>
         </div>
     </div>
